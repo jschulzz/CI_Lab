f4b644f6c5742bf81febeb06c3c98cc4d44d13de
John Schulz <jr.john.schulz@gmail.com>
2019-11-21 14:33:31 -0500

    Adding new Express Route

--- src/main/app.js
+++ src/main/app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     // the .status(200) isn't necessary since it is the default return status
     res.status(200).send('Hello World!')
 })
+app.get('/resources', (req, res) => {
+    // the .status(200) isn't necessary since it is the default return status
+    res.status(200).send('Resources!')
+})
 
 app.listen(3000, () => {
     console.log('Example app listening on http://localhost:3000')
